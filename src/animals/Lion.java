918b1505b8baf3ccfdf14a814a0d4e4f0fb49f08
Deniz <deniz.hashimov@mentormate.com>
2017-06-05 13:26:27 +0300

    Added example classes

--- src/animals/Lion.java
+++ src/animals/Lion.java
@@ -1,5 +1,22 @@
 package animals;
 
-public class Lion {
+import enums.Place;
+import enums.Type;
+import main.Animal;
+
+public class Lion extends Animal {
+	
+	static Lion self;
+	
+	public static Lion getInstance(){
+		if(self == null){
+			self = new Lion("Leo", Type.PREDATOR, "Some Description", Place.OPEN_SPACE);
+		}
+		return self;
+	}
+
+	private Lion(String name, Type type, String description, Place place) {
+		super(name, type, description, place);
+	}
 
 }
--- src/main/Animal.java
+++ src/main/Animal.java
@@ -1,5 +1,37 @@
 package main;
 
-public class Animal {
+import enums.Place;
+import enums.Type;
+
+public abstract class Animal {
+    
+	public String name;
+	public Type type;
+	public String description;
+	public Place place;
+	
+	public Animal(String name, Type type, String description, Place place) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.place = place;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Place getPlace() {
+		return place;
+	}
 
 }
