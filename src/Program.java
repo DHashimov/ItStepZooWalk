74965ce9cd103537d96b64c94f8d75298c36ecd8
Deniz <deniz.hashimov@mentormate.com>
2017-06-18 10:58:17 +0300

    added mysql connection library

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,41 @@
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
 		
+
+
 	}
+	
+	
+	//TODO CREATE DATABASE and get data from it
+//	try {
+//		Class.forName("com.mysql.jdbc.Driver").newInstance();
+//		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/it_step_zoo_walk", "root", "");
+//
+//		Statement st = con.createStatement();
+//		String sql = ("SELECT * FROM animals;");
+//		ResultSet rs = st.executeQuery(sql);
+//		if (rs.next()) {
+//			int id = rs.getInt("_id");
+//			String name = rs.getString("name");
+//			String type = rs.getString("type");
+//			System.out.println("ID = " + id + " NAME = " + name + " TYPE = " + type);
+//			con.close();
+//		}
+//
+//	} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	} catch (SQLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	} finally {
+//
+//	}
 }
